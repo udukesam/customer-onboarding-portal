8382d264b440b59dbd615ffe9c956330231d6783
Duke Sam Unnatha Arunothayam <udukesam@gmail.com>
2025-11-04 15:18:35 +0530

    Delete backend directory

--- backend/routes/payment.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.post('/initiate', (req, res) => {
-  const { company, amount } = req.body;
-  res.json({ company, amount, status: 'Payment processed successfully' });
-});
-
-export default router;
