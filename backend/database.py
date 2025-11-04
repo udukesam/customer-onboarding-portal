4594312bf533f4a7904db0eaa887f9ac4b4365bc
Duke Sam Unnatha Arunothayam <udukesam@gmail.com>
2025-11-04 16:22:43 +0530

    CFESTG-2: Implement Customer Registration Module with Form UI and Backend API

---
+++ backend/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
+
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+metadata = MetaData()
