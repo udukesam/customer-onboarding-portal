b97cbf329a2d6102a81afcc55b836b3f8b338295
Duke Sam Unnatha Arunothayam <udukesam@gmail.com>
2025-11-04 16:03:21 +0530

    Delete src directory

--- src/pages/RegistrationPage.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import RegistrationForm from '../components/RegistrationForm';
-
-export default function RegistrationPage(){
-    return (<div><RegistrationForm /></div>)
-}
